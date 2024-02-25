d2049a0147ad40e42a4fc88b6701ccc68662b281
Adebayo Awwal Bamidele <adebayoawwaldele@gmail.com>
2024-02-25 20:47:43 +0100

    initial commit

---
+++ Models/ItemsModels.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoProject.Models
+{
+    public class ItemsModels
+    { 
+        public Guid id { get; set; }
+        public string Name { get; set; }
+        public int Food { get; set; }
+        public int Price { get; set; }
+       
+    }
+}
